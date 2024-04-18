4c0f42dd8d62f7fc8c89a0db47d8cee4e0ac3cbb
IdleBear <idlebear@redmaples.ca>
2024-04-18 08:27:32 -0400

    environment setup

--- install-kernel.sh
+++ install-kernel.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+CONDA=$1
+if [ "$CONDA" = "" ]; then
+    CONDA="/home/$USER/conda.sh"
+fi
+
+source $CONDA
+
 conda create --yes --name waymo python=3.9
-conda install --yes -c anaconda ipykernel
+conda install --yes --name waymo -c anaconda ipykernel
+conda activate waymo
 pip install -r requirements.txt
-ipykernel install --user --name=waymo
+python -m ipykernel install --user --name=waymo
 
